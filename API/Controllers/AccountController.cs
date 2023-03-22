65180f592628794cdad423b5432ac1b00b8110f6
Prince bhati <79313705+bhati00@users.noreply.github.com>
2023-03-22 11:45:10 +0530

    created the endpoints login and register

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -2,6 +2,8 @@ using System.Security.Cryptography;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using API.DTO;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -13,18 +15,35 @@ namespace API.Controllers
             _context = context;
         }
         [HttpPost("register")] //Post request api/Account/register
-        public async Task<ActionResult<AppUser>> Register(String username,String password)
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
         {
+            if(await UserExists(registerDto.Username)) return BadRequest("username already exists");
             using var hmac = new HMACSHA512();
             var user = new AppUser
             {
-                UserName = username,
-                PasswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password)),
+                UserName = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key            
             };
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
             return user;
+        }
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto){
+            var user =  await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+            if(user == null) return Unauthorized("invlid username");
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var ComputedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(loginDto.Password));
+            for(int i=0; i < ComputedHash.Length; i++)
+            {
+                if(ComputedHash[i]!= user.PasswordHash[i]) return Unauthorized("invalid password");
+            }
+            return user;
+        }
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
 
 
--- docs/section4.md
+++ docs/section4.md
@@ -1,4 +1,20 @@
 **Safe storage of passwords**
 *a) using hasing ,using var hmac = new HMACSHA512()*
 *b)using hashing and salting(adds an extra value to the password before hashing)*
+
 **create a BaseApiController**
+*it will help us to write resuable code , testing, consistency*
+**Using a Debugger**
+*set  a breakpoint and attach it , then start the debugger*
+
+**WHY WE CREATE THE DTO's**
+ * encapsulate the data being transferred, ensure the format that is appropriate for the receiving client or server
+ * domain models may contain sensitive information
+
+**Adding the validation**
+*-validating the data in DTOs, you can ensure that the input received from the client is valid and meets the business requirements*
+*-when you put validations in the DTO, they are checked by the API controller when it receives data from the client*
+*-by putting validations in DTOs instead of entities in .NET Web API, you can achieve separation of concerns, reusability, security, flexibility, and performance*
+
+**creating endpoint login**
+*create a dto logindto and then create a post method for the login which compares the computed hash of input password and stored hash of the user*
