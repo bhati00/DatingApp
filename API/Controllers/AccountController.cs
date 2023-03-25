6e0b3aa88e6a9f39a22b3ff9da765dae87a3e461
Prince bhati <79313705+bhati00@users.noreply.github.com>
2023-03-25 22:58:02 +0530

    end of sectoin 3

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -4,42 +4,54 @@ using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using API.DTO;
 using Microsoft.EntityFrameworkCore;
+using API.Interface;
 
 namespace API.Controllers
 {
+    
     public class AccountController : BaseApiController
     {
         private readonly DataContext _context;
-        public AccountController(DataContext context)
+        private readonly ITokenService _tokenService;
+        public AccountController(DataContext context, ITokenService tokenService)
         {
+            _tokenService = tokenService;
             _context = context;
+
         }
         [HttpPost("register")] //Post request api/Account/register
-        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
-            if(await UserExists(registerDto.Username)) return BadRequest("username already exists");
+            if (await UserExists(registerDto.Username)) return BadRequest("username already exists");
             using var hmac = new HMACSHA512();
             var user = new AppUser
             {
                 UserName = registerDto.Username.ToLower(),
                 PasswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(registerDto.Password)),
-                PasswordSalt = hmac.Key            
+                PasswordSalt = hmac.Key
             };
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
-            return user;
+            return new UserDto{
+                UserName = registerDto.Username,
+                Token = _tokenService.CreateToken(user)
+            };
         }
         [HttpPost("login")]
-        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto){
-            var user =  await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
-            if(user == null) return Unauthorized("invlid username");
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+            if (user == null) return Unauthorized("invlid username");
             using var hmac = new HMACSHA512(user.PasswordSalt);
             var ComputedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(loginDto.Password));
-            for(int i=0; i < ComputedHash.Length; i++)
+            for (int i = 0; i < ComputedHash.Length; i++)
             {
-                if(ComputedHash[i]!= user.PasswordHash[i]) return Unauthorized("invalid password");
+                if (ComputedHash[i] != user.PasswordHash[i]) return Unauthorized("invalid password");
             }
-            return user;
+             return new UserDto{
+                UserName = loginDto.Username,
+                Token = _tokenService.CreateToken(user)
+            };
         }
         private async Task<bool> UserExists(string username)
         {
--- docs/section4.md
+++ docs/section4.md
@@ -18,3 +18,29 @@
 
 **creating endpoint login**
 *create a dto logindto and then create a post method for the login which compares the computed hash of input password and stored hash of the user*
+**JSON web tokens**
+*a standard for securely transmitting information between parties as a JSON object. It is commonly used in web applications as a way to authenticate and authorize users.*
+
+**WHY JWT  TOKENS NEDDED :**
+* Unlike traditional session-based authentication, which requires the server to store session data for each user, JWT is stateless
+* Cross-domain, Standardized, Security.
+
+*<u>Steps to create jwt token</u>*
+-->first create a interface and then it's service class , then register the service dependency injection 
+-->Read a secret key from an external configuration file(program.cs) or source using IConfiguration.
+-->Create a new SymmetricSecurityKey object with the secret key.
+-->Create a method named CreateToken that takes an AppUser instance as input and returns a JWT as a string.
+-->Create a list of claims with a single claim containing the AppUser's UserName.
+-->Create a new SigningCredentials object with the SymmetricSecurityKey and a signature algorithm (HmacSha512Signature).
+-->Create a new SecurityTokenDescriptor object with the list of claims, an expiration time (7 days from the current time), and the SigningCredentials object.
+-->Create a new JwtSecurityTokenHandler object.
+-->Call the CreateToken method on the JwtSecurityTokenHandler, passing in the SecurityTokenDescriptor object created earlier. This returns a new JWT.
+-->Call the WriteToken method on the JwtSecurityTokenHandler, passing in the new JWT. This returns the JWT as a string.
+
+**registers a service dependency injection in program.cs**
+*Once you've registered the service with the dependency injection container using the AddScoped method , you can use it in your application by injecting it into your classes or controllers that require it.*
+
+**Adding the Authentication middleware**
+-->Install the  Microsoft.AspNetCore.Authentication.JwtBearer for JWT authentication.
+-->use the UseAuthentication and UseAuthorization extension methods to add the middleware. The UseAuthentication middleware should be added before the UseAuthorization middleware.
+**adding the extension method : set the class as static**
